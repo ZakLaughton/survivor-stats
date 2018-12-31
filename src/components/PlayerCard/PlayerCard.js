6c787f4b5b5e21daebb5809cab8393ac4decfa02
Zak Laughton <zlaughton@gmail.com>
2018-12-30 20:16:17 -0600

    Add card grow on hover

--- src/components/PlayerCard/PlayerCard.js
+++ src/components/PlayerCard/PlayerCard.js
@@ -4,7 +4,7 @@ const PlayerCard = ({player}) => {
   const imageFileName = `${player.firstName}_${player.lastName}.jpg`.toLowerCase();
   console.log(imageFileName);
   return(
-    <article className="relative ma1 br2 ba dark-gray b--black-10 w-100 w-50-m w-25-l ma2">
+    <article className="grow relative ma1 br2 ba dark-gray b--black-10 w-100 w-50-m w-25-l ma2">
       <img
         src={require(`../../img/${imageFileName}`)}
         className="db w-100 h-100 br2 br--top"
