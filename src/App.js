08a64d17c98379e4158fa0aed636e9186b97c59f
Zak Laughton <zlaughton@gmail.com>
2018-12-30 18:14:39 -0600

    Dynamically pull in tribe data

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 import Tribe from './components/Tribe/Tribe';
 
+const players = require('./database/players.json');
+const tribes = require('./database/tribes.json');
+const episodes = require('./database/episodes.json');
+
 class App extends Component {
   render() {
+    console.log(players);
     return (
       <div className="App">
-        <Tribe name="David" color="orange"/>
-        <Tribe name="Goliath" color="purple"/>
+        {tribes.map(tribe => (
+            <Tribe
+              name={tribe.name}
+              color={tribe.tribeColor} />
+        ))}
       </div>
     );
   }
