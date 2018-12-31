98fbb9c13dcc96b213c25109b465a205e480bccb
Zak Laughton <zlaughton@gmail.com>
2018-12-30 19:41:04 -0600

    Move player name positioning

--- src/components/PlayerCard/PlayerCard.js
+++ src/components/PlayerCard/PlayerCard.js
@@ -2,12 +2,10 @@ import React from 'react';
 
 const PlayerCard = ({player}) => {
   return(
-    <article class="ma1 br2 ba dark-gray b--black-10 w-100 w-50-m w-25-l ma2">
-      <img src="http://placekitten.com/g/600/300" class="db w-100 br2 br--top" alt="Kitten looking menacing." />
-      <div class="dt w-100 mt1">
-        <div class="dtc">
-          <h2 class="f4-ns mv0">{player.fullName}</h2>
-        </div>
+    <article className="relative ma1 br2 ba dark-gray b--black-10 w-100 w-50-m w-25-l ma2">
+      <img src="http://placekitten.com/g/600/300" class="db w-100 h-100 br2 br--top" alt="Kitten looking menacing." />
+      <div className="w-100 absolute bottom-0 ">
+          <h2 className="bg-silver  f4-ns mv0 center tc">{player.fullName}</h2>
       </div>
     </article>
   )
