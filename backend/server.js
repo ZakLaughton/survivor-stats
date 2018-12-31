c731287b6299c657d594089045f64f247a9b5124
Zak Laughton <zlaughton@gmail.com>
2018-12-31 01:26:40 -0600

    Add endpoint for episode data gathering.

--- backend/server.js
+++ backend/server.js
@@ -9,3 +9,12 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('app is running on port 3000');
 })
+
+app.get('/s:season/e:episode', (req, res) => {
+  const { season, episode } = req.params;
+  res.json(`season ${season}, episode ${episode}`);
+})
+/*
+/sXX/eXX/ --> return players/status
+
+*/
