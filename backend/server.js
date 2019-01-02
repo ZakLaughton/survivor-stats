ffb0549b3218f34dc7ce56e899683ea10f8541f4
Zak Laughton <zlaughton@gmail.com>
2019-01-02 13:52:13 -0600

    Add dotenv to connect to set database env

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
+require('dotenv').load();
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const knex = require('knex');
+
 app.use(cors());
 
 const episodePlayerData = require('./database/castaways');
@@ -8,13 +12,23 @@ const tribeData=require('./database/tribes')
 const response = {
   castaways: episodePlayerData,
   tribes: tribeData}
-app.get('/', (req, res) => {
+  app.get('/', (req, res) => {
   console.log('res: ', response);
   res.status('200').json(response);
 })
 
+const db = knex({
+  client: 'pg',
+  connection: {
+    connectionString : process.env.DATABASE_URL,
+    ssl: true
+  }
+});
+
 app.listen(3000, () => {
   console.log('app is running on port 3000');
+  db.select('*').from('castaways')
+    .then((castaway) => console.log(castaway))
 })
 
 app.get('/s:season/e:episode', (req, res) => {episodePlayerData.getEpisodePlayerData(req, res)})
