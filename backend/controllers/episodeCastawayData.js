6106fab8a4b2e5b92a3a241685a0c3e096a09e32
Zak Laughton <zlaughton@gmail.com>
2019-01-02 18:00:09 -0600

    Return tribe and castaway info in response

--- backend/controllers/episodeCastawayData.js
+++ backend/controllers/episodeCastawayData.js
@@ -4,10 +4,28 @@ const episode = 1;
 const getEpisodeCastawayData = (req, res, db) => {
   // const { season, episode } = req.params;
   // res.json(`season ${season}, episode ${episode}`);
-  console.log(season, episode);
-  db.select('name').from('season_castaway_mapping')
-    .where('season_no', '=', 37)
-    .then((castaways) => console.log(castaways))
+  response = {
+    tribes: [],
+    castaways: []
+  }
+  db.select('name', 'tribe_color').from('tribes').where('season', '=', 37)
+    .then((seasonTribes) => {
+      response.tribes = seasonTribes;
+      db.select('full_name', 'updates.value').from('castaways')
+        .innerJoin('season_castaway_mapping', 'castaways.full_name', 'season_castaway_mapping.name')
+        .innerJoin('updates', 'castaways.full_name', 'updates.castaway')
+        .where('season_no', '=', 37)
+        .then((seasonCastaways) => {
+          response.castaways = seasonCastaways.map((castaway) => {
+            castawayObj = {name: '', tribe: ''}
+            castawayObj.name = castaway.full_name;
+            castawayObj.tribe = castaway.value;
+            return castawayObj
+          })
+          console.log(response)
+        })
+    })
+  
 }
 
 module.exports = {
