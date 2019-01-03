52f2da5324345f794463acd210cff20bc0fd71a9
Zak Laughton <zlaughton@gmail.com>
2019-01-03 09:20:15 -0600

    Change navbar to select fields

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -4,7 +4,7 @@ import './NavBar.css';
 const NavBar = ({season, episode}) => {
   return(
     <header class="navbar" id="myTopnav">
-      <div class="dropdown">
+      {/* <div class="dropdown">
         <button class="dropbtn">Season</button>
         <div class="dropdown-content">
           <a href="#">Link 1</a>
@@ -19,7 +19,27 @@ const NavBar = ({season, episode}) => {
           <a href="#">Link 2</a>
           <a href="#">Link 3</a>
         </div>
-      </div>
+      </div> */}
+      <select>
+        <option selected disabled>Season</option>
+        <option value="season37">Season 37</option>>
+      </select>
+      <select>
+        <option selected disabled>Episode</option>
+        <option value="episode1">Episode 1</option>
+        <option value="episode2">Episode 2</option>
+        <option value="episode3">Episode 3</option>
+        <option value="episode4">Episode 4</option>
+        <option value="episode5">Episode 5</option>
+        <option value="episode6">Episode 6</option>
+        <option value="episode7">Episode 7</option>
+        <option value="episode8">Episode 8</option>
+        <option value="episode9">Episode 9</option>
+        <option value="episode10">Episode 10</option>
+        <option value="episode11">Episode 11</option>
+        <option value="episode12">Episode 12</option>
+        <option value="episode13">Episode 13</option>
+      </select>
     </header>
   )
 }
