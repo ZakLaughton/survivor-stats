a60bc7ae22e96e9f24f77f359f21433ac2cf24cd
Zak Laughton <zlaughton@gmail.com>
2019-01-02 23:44:18 -0600

    Change calls to async/await syntax

--- backend/controllers/episodeCastawayData.js
+++ backend/controllers/episodeCastawayData.js
@@ -1,31 +1,27 @@
 const season = 37;
 const episode = 1;
 
-const getEpisodeCastawayData = (req, res, db) => {
+
+const getEpisodeCastawayData = async (req, res, db) => {
   // const { season, episode } = req.params;
   // res.json(`season ${season}, episode ${episode}`);
-  response = {
+  let response = {
     tribes: [],
     castaways: []
   }
-  db.select('name', 'tribe_color').from('tribes').where('season', '=', 37)
-    .then((seasonTribes) => {
-      response.tribes = seasonTribes;
-      db.select('full_name', 'updates.value').from('castaways')
-        .innerJoin('season_castaway_mapping', 'castaways.full_name', 'season_castaway_mapping.name')
-        .innerJoin('updates', 'castaways.full_name', 'updates.castaway')
-        .where('season_no', '=', 37)
-        .then((seasonCastaways) => {
-          response.castaways = seasonCastaways.map((castaway) => {
-            castawayObj = {name: '', tribe: ''}
-            castawayObj.name = castaway.full_name;
-            castawayObj.tribe = castaway.value;
-            return castawayObj
-          })
-          res.json(response);
-        })
-    })
+  response.tribes = await db.select('name', 'tribe_color').from('tribes').where('season', '=', 37)
+  const seasonCastaways = await db.select('full_name', 'updates.value').from('castaways')
+    .innerJoin('season_castaway_mapping', 'castaways.full_name', 'season_castaway_mapping.name')
+    .innerJoin('updates', 'castaways.full_name', 'updates.castaway')
+    .where('season_no', '=', 37)
   
+    response.castaways = seasonCastaways.map((castaway) => {
+      castawayObj = {name: '', tribe: ''}
+      castawayObj.name = castaway.full_name;
+      castawayObj.tribe = castaway.value;
+      return castawayObj
+    })
+    res.json(response);
 }
 
 module.exports = {
