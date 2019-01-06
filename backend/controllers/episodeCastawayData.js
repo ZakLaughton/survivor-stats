6546ab1a0f157639ebde83844b99c0da82a9be77
Zak Laughton <zlaughton@gmail.com>
2019-01-05 22:47:47 -0600

    Refactor: Set backend to return episode castaways

--- backend/controllers/episodeCastawayData.js
+++ backend/controllers/episodeCastawayData.js
@@ -1,19 +1,43 @@
-const getEpisodeCastawayData = async (req, res, db, season) => {
-  const { episode } = req.query;
-  const formattedEpisode = ("0" + episode).slice(-2);
+const getEpisodeCastawayData = async (req, res, db, season = 37) => {
 
   let response = {
+    season,
     tribes: [],
-    seasonTribeChanges: []
+    episodes: []
   }
 
   response.tribes = await db.select('name', 'tribe_color').from('tribes').where('season', '=', 37)
-  const seasonCastaways = await db.select('full_name').from('castaways')
-    .innerJoin('season_castaway_mapping', 'castaways.full_name', 'season_castaway_mapping.name')
-    .where('season_no', '=', season);
+  const seasonEpisodes = await db.select('*').from('episodes').where('id', 'like', `s${season}e%`);
+  const seasonCastaways = await db.select('*').from('season_castaway_mapping').where('season_no', '=', '37');
   const seasonTribeChanges = await db.select('castaway', 'field_value', 'start_episode').from('updates')
     .where('start_episode', 'like', 's37%');
-    
+  
+  // Data from seasonTribeChanges to fill out data for each episode
+  // [{id: sXXeXX,
+  //   castaways: [
+  //     {name: <name>,
+  //      tribe: <tribe>}
+  //   ]
+  // }, ...]
+
+  // Get all season episodes
+  // Get all castaways
+
+  // For each episode
+  //   for each castaway
+  //     get the current tribe
+
+  castawayDataByEpisode = seasonEpisodes.map((episode) => {
+    episodeObj = {
+      id: null,
+      castaways: []
+    }
+    episodeObj.id = Number(episode.id.slice(-2));
+    episodeObj.castaways = seasonCastaways.map((castaway) => {
+      return {'name': castaway.name}
+    })
+  })
+  console.log(JSON.stringify(episodeObj))
 
   // Make castaway properties clearly titled for JSON data return
   response.seasonTribeChanges = seasonTribeChanges;
