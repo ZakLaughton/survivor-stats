7789ca3490de08d64c756f7569497a57bfcf4b75
Zak Laughton <zlaughton@gmail.com>
2019-01-06 10:25:16 -0600

    Refactor: Update API to return tribe data

--- backend/controllers/episodeCastawayData.js
+++ backend/controllers/episodeCastawayData.js
@@ -10,37 +10,37 @@ const getEpisodeCastawayData = async (req, res, db, season = 37) => {
   const seasonEpisodes = await db.select('*').from('episodes').where('id', 'like', `s${season}e%`);
   const seasonCastaways = await db.select('*').from('season_castaway_mapping').where('season_no', '=', '37');
   const seasonTribeChanges = await db.select('castaway', 'field_value', 'start_episode').from('updates')
-    .where('start_episode', 'like', 's37%');
-  
-  // Data from seasonTribeChanges to fill out data for each episode
-  // [{id: sXXeXX,
-  //   castaways: [
-  //     {name: <name>,
-  //      tribe: <tribe>}
-  //   ]
-  // }, ...]
+    .where('start_episode', 'like', `s${season}%`);
 
-  // Get all season episodes
-  // Get all castaways
+  const castawayDataByEpisode = seasonEpisodes
+    .filter(episode => episode.air_date)
+    .map((episode) => {
+      const episodeObj = {
+        id: null,
+        castaways: []
+      }
+      episodeObj.id = `s${season}e${episode.id.slice(-2)}`;
+      episodeObj.castaways = seasonCastaways.map((castaway) => {
+        return {'name': castaway.name}
+      })
 
-  // For each episode
-  //   for each castaway
-  //     get the current tribe
+      // Populate each tribe for the current episode
+      const castawaysWithTribes = episodeObj.castaways.map((castaway) => {
+        const updatedCastaway = castaway;
+        const currentTribe = seasonTribeChanges
+          .filter(change => change.start_episode <= episodeObj.id)
+          .filter(currentChange => currentChange.castaway === castaway.name)
+          .reduce((prev, current) => (prev.start_episode > current.start_episode) ? prev : current)
+    
+        updatedCastaway.tribe = currentTribe.field_value;
+        return updatedCastaway;
+      });
 
-  castawayDataByEpisode = seasonEpisodes.map((episode) => {
-    episodeObj = {
-      id: null,
-      castaways: []
-    }
-    episodeObj.id = Number(episode.id.slice(-2));
-    episodeObj.castaways = seasonCastaways.map((castaway) => {
-      return {'name': castaway.name}
-    })
+      episodeObj.castaways = castawaysWithTribes;
+      return episodeObj;
   })
-  console.log(JSON.stringify(episodeObj))
 
-  // Make castaway properties clearly titled for JSON data return
-  response.seasonTribeChanges = seasonTribeChanges;
+  response.episodes = castawayDataByEpisode
   res.json(response);
 }
 
