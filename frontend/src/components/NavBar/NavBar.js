ee302c7aeb7a23e2f8f9d7d3f3baf8638480c869
Zak Laughton <zlaughton@gmail.com>
2019-01-10 23:22:43 -0600

    feat: Dynamically fill navbar episodes

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './NavBar.css';
 
-const NavBar = ({allSeasons, setSeason, setEpisode, seasonNum, episodeId}) => {
+const NavBar = ({allSeasons, setSeason, setEpisode, seasonNum, episodeId, seasonData}) => {
   return(
     <header className="navbar" id="myTopnav">
       {/* <div class="dropdown">
@@ -26,21 +26,11 @@ const NavBar = ({allSeasons, setSeason, setEpisode, seasonNum, episodeId}) => {
         ))}
       </select>
       <select value={Number(episodeId.slice(-2))} onChange={evt => setEpisode(evt.target.value)}>
-        <option selected value="0">Episode</option>
-        <option value="1">Episode 1</option>
-        <option value="2">Episode 2</option>
-        <option value="3">Episode 3</option>
-        <option value="4">Episode 4</option>
-        <option value="5">Episode 5</option>
-        <option value="6">Episode 6</option>
-        <option value="7">Episode 7</option>
-        <option value="8">Episode 8</option>
-        <option value="9">Episode 9</option>
-        <option value="10">Episode 10</option>
-        <option value="11">Episode 11</option>
-        <option value="12">Episode 12</option>
-        <option value="13">Episode 13</option>
-        <option value="14">Episode 14</option>
+        {seasonData.episodes && seasonData.episodes
+          .sort((a, b) => ((a.id < b.id) ? -1: 1))
+          .map((episode) => (
+            <option value={Number(episode.id.slice(-2))}>Episode {Number(episode.id.slice(-2))}</option>
+          ))}
       </select>
     </header>
   )
