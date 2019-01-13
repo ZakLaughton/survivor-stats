dc5385d830ab146abc4e360a31bdf01da901e1ae
Zak Laughton <zlaughton@gmail.com>
2019-01-12 18:45:00 -0600

    feat: fill in former tribe circle colors

--- frontend/src/components/CastawayCard/CastawayCard.js
+++ frontend/src/components/CastawayCard/CastawayCard.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './CastawayCard.css';
 
-const CastawayCard = ({castaway, color}) => {
+const CastawayCard = ({castaway, tribeData}) => {
   const imageFileName = castaway.name.replace(/\s/, '_').toLowerCase() + '.jpg';
   return(
     <article className="castaway-card grow relative ma1 br2 ba dark-gray b--black-10 ma2">
       <div className="tribe-circle-container">
-        {castaway.formerTribes.map(tribe => <div className='tribe-circle'></div>)}
+        {tribeData && castaway.formerTribes.map(formerTribe => {
+          const circleColor = tribeData.find(tribe => formerTribe === tribe.name).tribe_color;
+          return (
+            <div 
+              className={`tribe-circle`}
+              style={{backgroundColor: circleColor}}
+            />
+          )
+        })}
       </div>
       <img
         src={require(`../../img/${imageFileName}`)}
