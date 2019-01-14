e142a08508c8b73f9e316c2508bce4e170c5e58c
Zak Laughton <zlaughton@gmail.com>
2019-01-13 23:09:43 -0600

    fix: Fix red and yellow tribe gradients

--- frontend/src/components/Tribe/Tribe.js
+++ frontend/src/components/Tribe/Tribe.js
@@ -10,7 +10,8 @@ const Tribe = ({ tribe, episodeData, tribeData }) => {
     'green': 'linear-gradient(to bottom, #007100, #328e24, #53ac41, #72ca5e, #91ea7b)',
     'blue': 'linear-gradient(to bottom, #0055f1, #5270f6, #7a8cfa, #9ca8fd, #bcc4ff)',
     'black': 'linear-gradient(to bottom, #363636, #575757, #7b7b7b, #a1a1a1, #c8c8c8)',
-    'red': 'linear-gradient(to bottom, #ff0000, #dd0003, #bd0004, #9d0003, #7e0000);'
+    'red': 'linear-gradient(to bottom, #ff0000, #dd0003, #bd0004, #9d0003, #7e0000)',
+    'yellow': 'background-image: linear-gradient(to bottom, #ffff00, #c7cc03, #939b03, #646c02, #394000)'
   }
 
   const sectionStyle = {
