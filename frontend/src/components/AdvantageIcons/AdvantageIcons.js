ff2bb8fcb99199d9659e0b694d79cf39b88ce89f
Zak Laughton <zlaughton@gmail.com>
2019-01-13 18:23:58 -0600

    feat: Add s36 advantages

--- frontend/src/components/AdvantageIcons/AdvantageIcons.js
+++ frontend/src/components/AdvantageIcons/AdvantageIcons.js
@@ -3,16 +3,19 @@ import './AdvantageIcons.css';
 
 const AdvantageIcons = ({castaway}) => {
   const iconLibrary = {
-    'immunity idol': 'fa-shield-alt',
-    'idol nullifier': 'fa-ban',
-    'vote steal': 'fa-user-minus'
+    'immunity idol': 'fas fa-shield-alt',
+    'idol nullifier': 'fas fa-ban',
+    'vote steal': 'fas fa-user-minus',
+    'legacy advantage': 'fas fa-scroll',
+    'extra vote': 'fas fa-plus-circle',
+    'fake idol': 'fab fa-pagelines'
   };
 
   return(
     <div className="tribe-advantage-container">
       {castaway.advantages && castaway.advantages.map(advantage => {
         return (
-          <i className={`tooltip advantage fas ${iconLibrary[advantage.item]}`}>
+          <i className={`tooltip advantage ${iconLibrary[advantage.item]}`}>
             <span className="animate fadeIn tooltiptext">{advantage.item}</span>
           </i>
         )
