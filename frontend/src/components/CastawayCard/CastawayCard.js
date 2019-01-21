982c5300b2788c04c78aa2fedeb0d0718d8ef49e
Zak Laughton <zlaughton@gmail.com>
2019-01-21 07:27:44 -0600

    feat: highlight former tribemates with tribe color

--- frontend/src/components/CastawayCard/CastawayCard.js
+++ frontend/src/components/CastawayCard/CastawayCard.js
@@ -10,15 +10,17 @@ class CastawayCard extends React.Component {
   }
 
   updateFormerTribeHover = () => {
-    const highlightedStyle = {
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-      boxShadow: 'inset 0px 0px 20px 10px rgb(255,0,0,0.7)',
-      top: 0,
-      left: 0
-    }
-    if (this.props.formerTribeHighlight.active && !this.state.hoverFormerTribeActive && this.props.castaway.formerTribes.find(tribe => tribe === this.props.formerTribeHighlight.tribeName)) {
+    const {formerTribeHighlight, castaway, tribeData} = this.props;
+    if (formerTribeHighlight.active && !this.state.hoverFormerTribeActive && castaway.formerTribes.find(tribe => tribe === formerTribeHighlight.tribeName)) {
+      const activeTribeColor = tribeData.find((tribe) => tribe.name === formerTribeHighlight.tribeName.replace(/\d| /g, '')).tribe_color; 
+      const highlightedStyle = {
+        position: 'absolute',
+        width: '100%',
+        height: '100%',
+        boxShadow: `inset 0px 0px 20px 10px ${activeTribeColor}`,
+        top: 0,
+        left: 0
+      }
       this.setState({hoverFormerTribeStyle: highlightedStyle, hoverFormerTribeActive: true})
     } else if (!this.props.formerTribeHighlight.active && this.state.hoverFormerTribeActive) {
       this.setState({hoverFormerTribeStyle: {}, hoverFormerTribeActive: false})      
