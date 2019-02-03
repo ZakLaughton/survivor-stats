b37ad8880da227d9a5c58b64da510fcaca44c6a4
Zak Laughton <zlaughton@gmail.com>
2019-02-03 11:59:04 -0600

    feat: Add section title/background color

--- frontend/src/components/PreseasonStats/PreseasonStats.js
+++ frontend/src/components/PreseasonStats/PreseasonStats.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './PreseasonStats.css';
 
-const PreseasonStats = () => <article className="preseason-stats" />;
+const PreseasonStats = () => (
+  <article className="preseason-stats">
+    <h1>Preseason Stats</h1>
+  </article>
+);
 
 export default PreseasonStats;
