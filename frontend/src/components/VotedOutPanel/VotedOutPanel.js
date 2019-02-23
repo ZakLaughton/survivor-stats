f8b184b9658d280ba81cb91732b5e001a1404eee
Zak Laughton <zlaughton@gmail.com>
2019-02-23 11:14:23 -0600

    refactor: Change voted out panel position to sticky
    This will keep the voted out panel on the bottom of the section as more
    sections are added.

--- frontend/src/components/VotedOutPanel/VotedOutPanel.js
+++ frontend/src/components/VotedOutPanel/VotedOutPanel.js
@@ -11,49 +11,46 @@ const VotedOutPanel = ({ episodeData, formerTribeHighlight, tribeData }) => {
     episodeData.castaways.some(castaway => castaway.tribe === 'out' || castaway.currentBoot)
   ) {
     return (
-      <div>
-        <div className="clear-footer" />
-        <section className="voted-out-panel animated slideInUp">
-          <div className="castawayList votedout">
-            {castaways &&
-              castaways
-                .filter(
-                  castaway =>
-                    (castaway.tribe === 'out' || castaway.currentBoot) && !castaway.juryMember,
-                )
-                .sort((a, b) => a.bootOrder - b.bootOrder)
-                .map(castaway => {
-                  return (
-                    <CastawayCard
-                      key={castaway.name}
-                      castaway={castaway}
-                      formerTribeHighlight={formerTribeHighlight}
-                      classNames={`animated fadeIn prejury`}
-                      tribeData={tribeData}
-                      episodeId={episodeData.id}
-                    />
-                  );
-                })}
-            {juryStarted && <span className="jury-title">JURY</span>}
-            {juryStarted &&
-              castaways
-                .filter(castaway => castaway.juryMember)
-                .sort((a, b) => a.bootOrder - b.bootOrder)
-                .map(castaway => {
-                  return (
-                    <CastawayCard
-                      key={castaway.name}
-                      castaway={castaway}
-                      formerTribeHighlight={formerTribeHighlight}
-                      classNames={`animated fadeIn jury`}
-                      tribeData={tribeData}
-                      episodeId={episodeData.id}
-                    />
-                  );
-                })}
-          </div>
-        </section>
-      </div>
+      <section className="voted-out-panel animated slideInUp">
+        <div className="castawayList votedout">
+          {castaways &&
+            castaways
+              .filter(
+                castaway =>
+                  (castaway.tribe === 'out' || castaway.currentBoot) && !castaway.juryMember,
+              )
+              .sort((a, b) => a.bootOrder - b.bootOrder)
+              .map(castaway => {
+                return (
+                  <CastawayCard
+                    key={castaway.name}
+                    castaway={castaway}
+                    formerTribeHighlight={formerTribeHighlight}
+                    classNames={`animated fadeIn prejury`}
+                    tribeData={tribeData}
+                    episodeId={episodeData.id}
+                  />
+                );
+              })}
+          {juryStarted && <span className="jury-title">JURY</span>}
+          {juryStarted &&
+            castaways
+              .filter(castaway => castaway.juryMember)
+              .sort((a, b) => a.bootOrder - b.bootOrder)
+              .map(castaway => {
+                return (
+                  <CastawayCard
+                    key={castaway.name}
+                    castaway={castaway}
+                    formerTribeHighlight={formerTribeHighlight}
+                    classNames={`animated fadeIn jury`}
+                    tribeData={tribeData}
+                    episodeId={episodeData.id}
+                  />
+                );
+              })}
+        </div>
+      </section>
     );
   } else {
     return null;
