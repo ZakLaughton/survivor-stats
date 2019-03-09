bc751b3824798d40c0b30b7e14b40884d136aa46
Zak Laughton <zlaughton@gmail.com>
2019-03-09 08:34:46 -0600

    chore: NPM packages/dependencies

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,10 +2,9 @@ module.exports = {
   extends: 'airbnb',
   rules: {
     'linebreak-style': 'off',
-    'react/jsx-filename-extension': 'off',
+    'react/jsx-filename-extension': 'off'
   },
-  parser: 'babel-eslint',
   env: {
-    browser: true,
-  },
+    browser: true
+  }
 };
