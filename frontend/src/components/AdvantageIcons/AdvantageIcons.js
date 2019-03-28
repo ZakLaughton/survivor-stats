1b8b09a621ba637186fe056afd45e10d4d97cdcd
Zak Laughton <zlaughton@gmail.com>
2019-03-28 09:18:19 -0500

    UI: Update advantage icons

--- frontend/src/components/AdvantageIcons/AdvantageIcons.js
+++ frontend/src/components/AdvantageIcons/AdvantageIcons.js
@@ -9,22 +9,32 @@ const AdvantageIcons = ({ castaway }) => {
     'legacy advantage': 'fas fa-scroll',
     'extra vote': 'fas fa-plus-circle',
     'fake idol': 'fab fa-pagelines',
-    'multi-choice advantage': 'fas fa-arrows-alt',
+    'multi-choice advantage': 'fas fa-arrows-alt'
   };
 
   const pngLibrary = {
     'immunity idol': {
       alt: 'Immunity Idol',
-      filename: 'immunity_idol.png',
+      filename: 'immunity_idol.png'
     },
     'fake immunity idol': {
       alt: 'Fake Immunity Idol',
-      filename: 'fake_immunity_idol.png',
+      filename: 'fake_immunity_idol.png'
     },
+    // half immunity idol deprecated, replaced with 1 & 2
+    // TODO: Remove after 1 & 2 in place
     'half immunity idol': {
       alt: 'Half Immunity Idol',
-      filename: 'half_immunity_idol.png',
+      filename: 'half_immunity_idol.png'
     },
+    'half immunity idol 1': {
+      alt: 'Half Immunity Idol',
+      filename: 'half_immunity_idol_1.png'
+    },
+    'half immunity idol 2': {
+      alt: 'Half Immunity Idol',
+      filename: 'half_immunity_idol_2.png'
+    }
   };
 
   /**
@@ -33,31 +43,34 @@ const AdvantageIcons = ({ castaway }) => {
    * @param {string} advantageName
    * @return {element}
    */
-  const getAdvantageIconElement = (advantageName) => {
+  const getAdvantageIconElement = advantageName => {
     if (
-      advantageName === 'fake idol'
-      || advantageName === 'immunity idol'
-      || advantageName === 'half immunity idol'
+      advantageName === 'fake idol' ||
+      advantageName === 'immunity idol' ||
+      advantageName === 'half immunity idol'
     ) {
       const advantage = pngLibrary[advantageName];
       return (
-        <div className="tooltip advantage">
-          <img alt={advantage.alt} src={require(`../../img/${advantage.filename}`)} />
-          <span className="animated fadeIn tooltiptext">{advantage.alt}</span>
+        <div className='tooltip advantage'>
+          <img
+            alt={advantage.alt}
+            src={require(`../../img/${advantage.filename}`)}
+          />
+          <span className='animated fadeIn tooltiptext'>{advantage.alt}</span>
         </div>
       );
     }
     return (
       <i className={`tooltip advantage ${iconLibrary[advantageName]}`}>
-        <span className="animated fadeIn tooltiptext">{advantageName}</span>
+        <span className='animated fadeIn tooltiptext'>{advantageName}</span>
       </i>
     );
   };
 
   return (
-    <div className="tribe-advantage-container">
-      {castaway.advantages
-        && castaway.advantages.map((advantage, index) => (
+    <div className='tribe-advantage-container'>
+      {castaway.advantages &&
+        castaway.advantages.map((advantage, index) => (
           <div key={index}>{getAdvantageIconElement(advantage.item)}</div>
         ))}
     </div>
