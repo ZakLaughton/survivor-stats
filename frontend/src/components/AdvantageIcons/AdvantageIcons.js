22dd11e5c268ded7b7f72e1b33944f9930b269c5
Zak Laughton <zlaughton@gmail.com>
2019-03-27 20:02:54 -0500

    feat: add advantage icons

--- frontend/src/components/AdvantageIcons/AdvantageIcons.js
+++ frontend/src/components/AdvantageIcons/AdvantageIcons.js
@@ -12,6 +12,21 @@ const AdvantageIcons = ({ castaway }) => {
     'multi-choice advantage': 'fas fa-arrows-alt',
   };
 
+  const pngLibrary = {
+    'immunity idol': {
+      alt: 'Immunity Idol',
+      filename: 'immunity_idol.png',
+    },
+    'fake immunity idol': {
+      alt: 'Fake Immunity Idol',
+      filename: 'fake_immunity_idol.png',
+    },
+    'half immunity idol': {
+      alt: 'Half Immunity Idol',
+      filename: 'half_immunity_idol.png',
+    },
+  };
+
   /**
    * Takes an advantage name and returns the HTML element to render the icon.
    * NOTE: All icons use fontawesome EXCEPT the fake idol, which uses a .png
@@ -19,11 +34,16 @@ const AdvantageIcons = ({ castaway }) => {
    * @return {element}
    */
   const getAdvantageIconElement = (advantageName) => {
-    if (advantageName === 'fake idol') {
+    if (
+      advantageName === 'fake idol'
+      || advantageName === 'immunity idol'
+      || advantageName === 'half idol'
+    ) {
+      const advantage = pngLibrary[advantageName];
       return (
         <div className="tooltip advantage">
-          <img alt="Fake Immunity Idol" src={require('../../img/fake_immunity_idol.png')} />
-          <span className="animated fadeIn tooltiptext">{advantageName}</span>
+          <img alt={advantage.alt} src={require(`../../img/${advantage.filename}`)} />
+          <span className="animated fadeIn tooltiptext">{advantage.alt}</span>
         </div>
       );
     }
