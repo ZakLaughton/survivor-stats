1fba10cb8182988dcb0099ac84b687f9dec960a5
Zak Laughton <zlaughton@gmail.com>
2019-04-12 20:55:41 -0500

    feature: Update wording for episode 0

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -12,9 +12,11 @@ const NavBar = ({
   seasonData,
   atEarliestEpisode,
   atLatestEpisode,
-}) => (
-  <header className="navbar" id="myTopnav">
-    {/* <div class="dropdown">
+}) => {
+  const episodeNumber = Number(episodeId.slice(-2));
+  return (
+    <header className="navbar" id="myTopnav">
+      {/* <div class="dropdown">
         <button class="dropbtn">Season</button>
         <div class="dropdown-content">
           <a href="#">Link 1</a>
@@ -30,33 +32,34 @@ const NavBar = ({
           <a href="#">Link 3</a>
         </div>
       </div> */}
-    <select
-      value={seasonNum || ''}
-      onChange={evt => setSeason(evt.target.value)}
-      id="season-select"
-    >
-      {allSeasons.map(season => (
-        <option key={season.season_no} value={season.season_no}>
-          {'s'}
-          {season.season_no.toString()}
-          {': '}
-          {season.title}
-        </option>
-      ))}
-    </select>
-    <div className="episode-selector">
-      <i
-        className={`fas fa-caret-left episode-arrow left ${atEarliestEpisode() && 'hidden'}`}
-        onClick={decrementEpisode}
-      />
-      <h2>{`EPISODE ${Number(episodeId.slice(-2))}`}</h2>
-      <i
-        className={`fas fa-caret-right episode-arrow right ${atLatestEpisode() && 'hidden'}`}
-        onClick={incrementEpisode}
-      />
-    </div>
-    <h1>Survivor Stats</h1>
-  </header>
-);
+      <select
+        value={seasonNum || ''}
+        onChange={evt => setSeason(evt.target.value)}
+        id="season-select"
+      >
+        {allSeasons.map(season => (
+          <option key={season.season_no} value={season.season_no}>
+            {'s'}
+            {season.season_no.toString()}
+            {': '}
+            {season.title}
+          </option>
+        ))}
+      </select>
+      <div className="episode-selector">
+        <i
+          className={`fas fa-caret-left episode-arrow left ${atEarliestEpisode() && 'hidden'}`}
+          onClick={decrementEpisode}
+        />
+        <h2>{episodeNumber === 0 ? 'START' : `EPISODE ${episodeNumber}`}</h2>
+        <i
+          className={`fas fa-caret-right episode-arrow right ${atLatestEpisode() && 'hidden'}`}
+          onClick={incrementEpisode}
+        />
+      </div>
+      <h1>Survivor Stats</h1>
+    </header>
+  );
+};
 
 export default NavBar;
