47e9545b75bcd60f8641af407229f1a2ce33da98
Zak Laughton <zlaughton@gmail.com>
2019-04-14 10:14:06 -0500

    feat: Set up season dropdown

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -16,36 +16,21 @@ const NavBar = ({
   const episodeNumber = Number(episodeId.slice(-2));
   return (
     <header className="navbar" id="myTopnav">
-      {/* <div class="dropdown">
-        <button class="dropbtn">Season</button>
-        <div class="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
+      {
+        <div className="dropdown" id="season-select">
+          <button className="dropbtn">Season</button>
+          <div className="dropdown-content">
+            {allSeasons.map(season => (
+              <a href="#" key={season.season_no} value={season.season_no}>
+                {season.season_no.toString()}
+                {': '}
+                {season.title}
+              </a>
+            ))}
+          </div>
         </div>
-      </div>
-      <div class="dropdown">
-        <button class="dropbtn">Episode</button>
-        <div class="dropdown-content">
-          <a href="#">Link 1</a>
-          <a href="#">Link 2</a>
-          <a href="#">Link 3</a>
-        </div>
-      </div> */}
-      <select
-        value={seasonNum || ''}
-        onChange={evt => setSeason(evt.target.value)}
-        id="season-select"
-      >
-        {allSeasons.map(season => (
-          <option key={season.season_no} value={season.season_no}>
-            {'s'}
-            {season.season_no.toString()}
-            {': '}
-            {season.title}
-          </option>
-        ))}
-      </select>
+      }
+
       <div className="episode-selector">
         <i
           className={`fas fa-caret-left episode-arrow left ${atEarliestEpisode() && 'hidden'}`}
