f314fa814e583c74cf3dbae28b9b2a9e4aed0108
Zak Laughton <zlaughton@gmail.com>
2019-04-17 19:51:58 -0500

    feat: Order tribal councils

--- frontend/src/components/TribalCouncils/TribalCouncils.js
+++ frontend/src/components/TribalCouncils/TribalCouncils.js
@@ -9,26 +9,28 @@ const TribalCouncils = ({ tribalCouncils, seasonNumber }) => {
   return (
     <section className="tribal-councils">
       <h1>{!multipleTribals && tribalCouncils[0].finalTribal ? 'Final Tribal' : tribalHeader}</h1>
-      <div className="tribal-councils-container">      
-        {tribalCouncils.map(tribalCouncil => (
-          <div className="tribal-council" key={tribalCouncil.tribalNumber}>
-            {!tribalCouncil.finalTribal && <h2>{`Day ${tribalCouncil.day}`}</h2>}
-            <p className="tribal-notes">{tribalCouncil.notes}</p>
-            {!tribalCouncil.fireMakingTribal
-              && tribalCouncil.vote_rounds.map((voteRound) => {
-                const roundNo = tribalCouncil.vote_rounds.length > 1 ? voteRound.round_no : null;
-                return (
-                  <TribalVotes
-                    key={roundNo}
-                    roundNo={roundNo}
-                    voteData={voteRound}
-                    votedFor={tribalCouncil.votedFor}
-                    seasonNumber={seasonNumber}
-                  />
-                );
-              })}
-          </div>
-        ))}
+      <div className="tribal-councils-container">
+        {tribalCouncils
+          .sort((a, b) => a.tribal_number - b.tribal_number)
+          .map(tribalCouncil => (
+            <div className="tribal-council" key={tribalCouncil.tribalNumber}>
+              {!tribalCouncil.finalTribal && <h2>{`Day ${tribalCouncil.day}`}</h2>}
+              <p className="tribal-notes">{tribalCouncil.notes}</p>
+              {!tribalCouncil.fireMakingTribal
+                && tribalCouncil.vote_rounds.map((voteRound) => {
+                  const roundNo = tribalCouncil.vote_rounds.length > 1 ? voteRound.round_no : null;
+                  return (
+                    <TribalVotes
+                      key={roundNo}
+                      roundNo={roundNo}
+                      voteData={voteRound}
+                      votedFor={tribalCouncil.votedFor}
+                      seasonNumber={seasonNumber}
+                    />
+                  );
+                })}
+            </div>
+          ))}
       </div>
     </section>
   );
