25b9a946652cd5ea24e506f30f82774fcafc3e1b
Zak Laughton <zlaughton@gmail.com>
2019-08-20 07:59:36 -0500

    fix linter errors

--- frontend/src/components/VotedOutPanel/VotedOutPanel.js
+++ frontend/src/components/VotedOutPanel/VotedOutPanel.js
@@ -1,60 +1,55 @@
 import React from 'react';
 import CastawayCard from '../CastawayCard/CastawayCard';
+
 import('./VotedOutPanel.css');
 
 const VotedOutPanel = ({ episodeData, formerTribeHighlight, tribeData }) => {
   const { castaways } = episodeData;
-  const juryStarted = castaways && castaways.some(castaway => castaway.juryMember) ? true : false;
+  const juryStarted = !!(castaways && castaways.some(castaway => castaway.juryMember));
 
   if (
-    episodeData.castaways &&
-    episodeData.castaways.some(castaway => castaway.tribe === 'out' || castaway.currentBoot)
+    episodeData.castaways
+    && episodeData.castaways.some(castaway => castaway.tribe === 'out' || castaway.currentBoot)
   ) {
     return (
       <section className="voted-out-panel animated slideInUp">
         <div className="castawayList votedout">
-          {castaways &&
-            castaways
+          {castaways
+            && castaways
               .filter(
-                castaway =>
-                  (castaway.tribe === 'out' || castaway.currentBoot) && !castaway.juryMember,
+                castaway => (castaway.tribe === 'out' || castaway.currentBoot) && !castaway.juryMember,
               )
               .sort((a, b) => a.bootOrder - b.bootOrder)
-              .map(castaway => {
-                return (
-                  <CastawayCard
-                    key={castaway.name}
-                    castaway={castaway}
-                    formerTribeHighlight={formerTribeHighlight}
-                    classNames={`animated fadeIn prejury`}
-                    tribeData={tribeData}
-                    episodeId={episodeData.id}
-                  />
-                );
-              })}
+              .map(castaway => (
+                <CastawayCard
+                  key={castaway.name}
+                  castaway={castaway}
+                  formerTribeHighlight={formerTribeHighlight}
+                  classNames="animated fadeIn prejury"
+                  tribeData={tribeData}
+                  episodeId={episodeData.id}
+                />
+              ))}
           {juryStarted && <span className="jury-title">JURY</span>}
-          {juryStarted &&
-            castaways
+          {juryStarted
+            && castaways
               .filter(castaway => castaway.juryMember)
               .sort((a, b) => a.bootOrder - b.bootOrder)
-              .map(castaway => {
-                return (
-                  <CastawayCard
-                    key={castaway.name}
-                    castaway={castaway}
-                    formerTribeHighlight={formerTribeHighlight}
-                    classNames={`animated fadeIn jury`}
-                    tribeData={tribeData}
-                    episodeId={episodeData.id}
-                  />
-                );
-              })}
+              .map(castaway => (
+                <CastawayCard
+                  key={castaway.name}
+                  castaway={castaway}
+                  formerTribeHighlight={formerTribeHighlight}
+                  classNames="animated fadeIn jury"
+                  tribeData={tribeData}
+                  episodeId={episodeData.id}
+                />
+              ))}
         </div>
       </section>
     );
-  } else {
-    return null;
   }
+  return null;
 };
 
 export default VotedOutPanel;
