591c544ef98450825a0bd3bd996ee4d80c8055a7
Zak Laughton <zlaughton@gmail.com>
2019-08-19 22:12:18 -0500

    eslint fixes

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-env jest */
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
