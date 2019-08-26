2633fa2b66035af9b64538cf03b830d478c8d4ed
Zak Laughton <zlaughton@gmail.com>
2019-08-26 09:43:28 -0500

    Merge in changes from styled-components
    Pulled in CastawayCard.js changes, including change to functional
    component and global context

--- frontend/src/components/CastawayCard/CastawayCard.js
+++ frontend/src/components/CastawayCard/CastawayCard.js
@@ -1,10 +1,11 @@
 /* eslint-disable global-require */
 /* eslint-disable import/no-dynamic-require */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './CastawayCard.css';
 import styled from 'styled-components';
 import AdvantageIcons from '../AdvantageIcons/AdvantageIcons';
 import FormerTribeIndicator from '../FormerTribeIndicator/FormerTribeIndicator';
+import { FormerTribeHighlightContext } from '../TribeBoard/TribeBoard';
 
 const FormerTribeShadow = ({ activeTribeColor }) => {
   const Wrapper = styled.div`
@@ -19,11 +20,18 @@ const FormerTribeShadow = ({ activeTribeColor }) => {
   `;
   return <Wrapper />;
 };
-class CastawayCard extends React.Component {
-  state = {
-    hoverFormerTribeStyle: {},
-    hoverFormerTribeActive: false,
-  };
+
+const CastawayCard = ({
+  castaway,
+  classNames,
+  tribeData,
+  formerTribeHighlight,
+  setFormerTribeHighlight,
+  removeFormerTribeHighlight,
+  episodeId,
+}) => {
+  const [hoverFormerTribeStyle, setHoverFormerTribeStyle] = useState({});
+  const [hoverFormerTribeActive, setHoverFormerTribeActive] = useState(false);
 
   /**
    * For seasons in which a tribe keeps the same name throughout multiple
@@ -31,7 +39,7 @@ class CastawayCard extends React.Component {
    * "Malolo 3") and returns a dictionary of their semantic names (e.g.
    * "Original Malolo", "Malolo (1st swap)", "Malolo (2nd Swap)')
    */
-  getSemanticTribeNames = (formerTribeList) => {
+  const getSemanticTribeNames = (formerTribeList) => {
     const semanticDictionary = {};
 
     formerTribeList.forEach((tribe) => {
@@ -41,97 +49,101 @@ class CastawayCard extends React.Component {
     return semanticDictionary;
   };
 
-  updateFormerTribeHover = () => {
-    const { formerTribeHighlight, castaway, tribeData } = this.props;
-    const { hoverFormerTribeActive } = this.state;
-    if (
-      formerTribeHighlight
-      && formerTribeHighlight.active
-      && formerTribeHighlight.tribeName !== 'Extinction Island'
-      && !hoverFormerTribeActive
-      && castaway.formerTribes.find(tribe => tribe === formerTribeHighlight.tribeName)
-    ) {
-      const activeTribeColor = tribeData.find(
-        tribe => tribe.name === formerTribeHighlight.tribeName.replace(/\d| /g, ''),
-      ).tribe_color;
-      this.setState({
-        hoverFormerTribeActive: true,
-      });
-    } else if (formerTribeHighlight && !formerTribeHighlight.active && hoverFormerTribeActive) {
-      this.setState({
-        hoverFormerTribeStyle: {},
-        hoverFormerTribeActive: false,
-      });
-    }
-  };
+  // useEffect(() => {
+  //   setFormerTribeHighlight((formerTribe) => {
+  //     if (
+  //       formerTribeHighlight
+  //       && formerTribeHighlight.active
+  //       && formerTribeHighlight.tribeName !== 'Extinction Island'
+  //       && !hoverFormerTribeActive
+  //       && castaway.formerTribes.find(tribe => tribe === formerTribeHighlight.tribeName)
+  //     ) {
+  //       const activeTribeColor = tribeData.find(
+  //         tribe => tribe.name === formerTribeHighlight.tribeName.replace(/\d| /g, ''),
+  //       ).tribe_color;
+  //       const highlightedStyle = {
+  //         boxShadow: `inset 0px 0px 20px 10px ${activeTribeColor}`,
+  //       };
+  //       this.setState({
+  //         hoverFormerTribeStyle: highlightedStyle,
+  //         hoverFormerTribeActive: true,
+  //       });
+  //     } else if (formerTribeHighlight && !formerTribeHighlight.active && hoverFormerTribeActive) {
+  //       this.setState({
+  //         hoverFormerTribeStyle: {},
+  //         hoverFormerTribeActive: false,
+  //       });
+  //     }
+  //   });
+  //   return {
+  //     active: false,
+  //     tribeName: null,
+  //   };
+  // }, [
+  //   castaway.formerTribes,
+  //   formerTribeHighlight,
+  //   hoverFormerTribeActive,
+  //   setFormerTribeHighlight,
+  //   tribeData,
+  // ]);
 
-  render() {
-    const {
-      castaway,
-      classNames,
-      tribeData,
-      formerTribeHighlight,
-      setFormerTribeHighlight,
-      removeFormerTribeHighlight,
-      episodeId,
-    } = this.props;
-    const activeTribeColor = null;
-    const imageFileName = `${episodeId.substring(0, 3)}_${castaway.name
-      .replace(/,|\./g, '')
-      .replace(/\s/g, '_')
-      .toLowerCase()}.jpg`;
-    const formerTribeClassNames = castaway.formerTribes
-      .map(formerTribe => `former-${formerTribe.replace(/\s/g, '-').toLowerCase()}`)
-      .join(' ');
+  const imageFileName = `${episodeId.substring(0, 3)}_${castaway.name
+    .replace(/,|\./g, '')
+    .replace(/\s/g, '_')
+    .toLowerCase()}.jpg`;
+  const formerTribeClassNames = castaway.formerTribes
+    .map(formerTribe => `former-${formerTribe.replace(/\s/g, '-').toLowerCase()}`)
+    .join(' ');
 
-    const semanticTribes = this.getSemanticTribeNames(castaway.formerTribes);
+  const semanticTribes = getSemanticTribeNames(castaway.formerTribes);
 
-    this.updateFormerTribeHover();
-
-    return (
-      <div
-        className={`castaway-card grow relative ma1 br2 ba dark-gray
+  return (
+    <FormerTribeHighlightContext.Consumer>
+      {context => (
+        <div
+          className={`castaway-card grow relative ma1 br2 ba dark-gray
           b--black-10 ma2 ${formerTribeClassNames} ${classNames}`}
-      >
-        <div className="tribe-circle-container">
-          {tribeData
-            && castaway.formerTribes
-            && castaway.formerTribes.map((formerTribe) => {
-              const circleColor = tribeData.find(
-                tribe => formerTribe.replace(/ \d/g, '') === tribe.name,
-              ).tribe_color;
-              return (
-                <FormerTribeIndicator
-                  key={formerTribe}
-                  circleColor={circleColor}
-                  formerTribe={formerTribe}
-                  setFormerTribeHighlight={setFormerTribeHighlight}
-                  removeFormerTribeHighlight={removeFormerTribeHighlight}
-                  formerTribeHighlight={formerTribeHighlight}
-                  semanticTribes={semanticTribes}
-                />
-              );
-            })}
+        >
+          <div className="tribe-circle-container">
+            {tribeData
+              && castaway.formerTribes
+              && castaway.formerTribes.map((formerTribe) => {
+                const circleColor = tribeData.find(
+                  tribe => formerTribe.replace(/ \d/g, '') === tribe.name,
+                ).tribe_color;
+                return (
+                  <FormerTribeIndicator
+                    key={formerTribe}
+                    circleColor={circleColor}
+                    formerTribe={formerTribe}
+                    setFormerTribeHighlight={setFormerTribeHighlight}
+                    removeFormerTribeHighlight={removeFormerTribeHighlight}
+                    formerTribeHighlight={formerTribeHighlight}
+                    semanticTribes={semanticTribes}
+                  />
+                );
+              })}
+          </div>
+          <AdvantageIcons castaway={castaway} />
+          <a href={castaway.wikiUrl} target="_blank" rel="noopener noreferrer">
+            <img
+              src={require(`../../img/${imageFileName}`)}
+              className="db br2 br--top"
+              alt={castaway.name}
+            />
+          </a>
+          <div className="shadow" style={hoverFormerTribeStyle} />
+          <div className="card-nameplate">
+            <h2 className="card-name br2 mv0 center tc">
+              {castaway.nickname
+                ? castaway.nickname
+                : castaway.name.substr(0, castaway.name.indexOf(' '))}
+            </h2>
+          </div>
         </div>
-        <AdvantageIcons castaway={castaway} />
-        <a href={castaway.wikiUrl} target="_blank" rel="noopener noreferrer">
-          <img
-            src={require(`../../img/${imageFileName}`)}
-            className="db br2 br--top"
-            alt={castaway.name}
-          />
-        </a>
-        <FormerTribeShadow activeTribeColor={activeTribeColor} />
-        <div className="card-nameplate">
-          <h2 className="card-name br2 mv0 center tc">
-            {castaway.nickname
-              ? castaway.nickname
-              : castaway.name.substr(0, castaway.name.indexOf(' '))}
-          </h2>
-        </div>
-      </div>
-    );
-  }
-}
+      )}
+    </FormerTribeHighlightContext.Consumer>
+  );
+};
 
 export default CastawayCard;
