623c72a8726afb4064b0a4877413b2d4e2bcd145
Zak Laughton <zlaughton@gmail.com>
2019-08-26 22:54:53 -0500

    move votedoutpanel to styled-components

--- frontend/src/components/VotedOutPanel/VotedOutPanel.js
+++ frontend/src/components/VotedOutPanel/VotedOutPanel.js
@@ -1,19 +1,62 @@
 import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 import CastawayCard from '../CastawayCard/CastawayCard';
 
-import('./VotedOutPanel.css');
+const GlobalStyle = createGlobalStyle`
+  .votedout .prejury img {
+    filter: grayscale(100%);
+  }
+
+  .voted-out-panel img {
+    width: 64px;
+  }
+
+  .voted-out-panel .card-nameplate {
+    display:none;
+  }
+
+  .voted-out-panel .tribe-circle-container {
+    display: none;
+  }
+`;
 
 const VotedOutPanel = ({ episodeData, formerTribeHighlight, tribeData }) => {
   const { castaways } = episodeData;
   const juryStarted = !!(castaways && castaways.some(castaway => castaway.juryMember));
+
+  const Wrapper = styled.section`
+    background-color: #333;
+    position: sticky;
+    bottom: 0;
+    width: 100%;
+    z-index: 1;
+    height: 80px;
+  `;
+
+  const VotedOutList = styled.div`
+    height: 100%;
+    justify-content: flex-start;
+    flex-flow: row;
+    overflow-x: auto;
+    overflow-y: hidden;
+  `;
+
+  const JuryTitle = styled.span`
+    writing-mode: tb-rl;
+    transform: rotate(-180deg);
+    font-family: 'Londrina Solid', sans-serif;
+    font-weight: 350;
+    color: white;
+  `;
 
   if (
     episodeData.castaways
     && episodeData.castaways.some(castaway => castaway.tribe === 'out' || castaway.currentBoot)
   ) {
     return (
-      <section className="voted-out-panel animated slideInUp">
-        <div className="castawayList votedout">
+      <Wrapper className="voted-out-panel animated slideInUp">
+        <GlobalStyle />
+        <VotedOutList className="castawayList votedout">
           {castaways
             && castaways
               .filter(
@@ -24,13 +67,12 @@ const VotedOutPanel = ({ episodeData, formerTribeHighlight, tribeData }) => {
                 <CastawayCard
                   key={castaway.name}
                   castaway={castaway}
-                  formerTribeHighlight={formerTribeHighlight}
                   classNames="animated fadeIn prejury"
                   tribeData={tribeData}
                   episodeId={episodeData.id}
                 />
               ))}
-          {juryStarted && <span className="jury-title">JURY</span>}
+          {juryStarted && <JuryTitle>JURY</JuryTitle>}
           {juryStarted
             && castaways
               .filter(castaway => castaway.juryMember)
@@ -45,8 +87,8 @@ const VotedOutPanel = ({ episodeData, formerTribeHighlight, tribeData }) => {
                   episodeId={episodeData.id}
                 />
               ))}
-        </div>
-      </section>
+        </VotedOutList>
+      </Wrapper>
     );
   }
   return null;
