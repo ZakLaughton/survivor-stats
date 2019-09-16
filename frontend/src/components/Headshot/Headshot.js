eb5c69d2fc65d5d3439f3ad5053f75f6bec3b31e
Zak Laughton <zlaughton@gmail.com>
2019-09-15 23:50:00 -0500

    transferred headshots to cloudinary

--- frontend/src/components/Headshot/Headshot.js
+++ frontend/src/components/Headshot/Headshot.js
@@ -2,15 +2,19 @@
 /* eslint-disable global-require */
 import React from "react";
 import styled from "styled-components";
+import { Image, Transformation } from "cloudinary-react";
 
 const Headshot = ({ seasonNumber, castaway }) => {
-  const headshotImage = require(`../../img/s${seasonNumber}_${castaway
-    .replace(/\s/, `_`)
-    .toLowerCase()}_headthumb.png`);
+  const imageFileName = `s${seasonNumber}_${castaway
+    .replace(/,|\./g, ``)
+    .replace(/\s/g, `_`)
+    .toLowerCase()}`;
 
   return (
     <HeadshotContainer>
-      <HeadshotImage alt={castaway} src={headshotImage} title={castaway} />
+      <Image publicId={`castaways/${imageFileName}`}>
+        <Transformation gravity="face" height="70" width="70" crop="thumb" />
+      </Image>
     </HeadshotContainer>
   );
 };
@@ -18,9 +22,6 @@ const Headshot = ({ seasonNumber, castaway }) => {
 const HeadshotContainer = styled.div`
   width: 70px;
   height: 70px;
-`;
-
-const HeadshotImage = styled.img`
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.16), 0 2px 10px 0 rgba(0, 0, 0, 0.12) !important;
 `;
 
