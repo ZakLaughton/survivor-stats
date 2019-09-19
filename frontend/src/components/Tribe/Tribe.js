362cb00c10fe5141193af2de1b736614b7cc8c85
Zak Laughton <zlaughton@gmail.com>
2019-09-18 20:04:49 -0500

    update castaway card colors

--- frontend/src/components/Tribe/Tribe.js
+++ frontend/src/components/Tribe/Tribe.js
@@ -10,20 +10,6 @@ const Tribe = ({
   setFormerTribeHighlight,
   removeFormerTribeHighlight,
 }) => {
-  // mycolor.space gradient. First color, then first color on "switch palette"
-  const backgroundGradients = {
-    orange: `linear-gradient(to bottom, #df940a, #be710e, #9b510e, #76340a, #511a00)`,
-    purple: `linear-gradient(to bottom, #740274, #963595, #b85bb7, #db80db, #ffa5ff)`,
-    green: `linear-gradient(to bottom, #007100, #328e24, #53ac41, #72ca5e, #91ea7b)`,
-    blue: `linear-gradient(to bottom, #0055f1, #5270f6, #7a8cfa, #9ca8fd, #bcc4ff)`,
-    black: `linear-gradient(to bottom, #363636, #575757, #7b7b7b, #a1a1a1, #c8c8c8)`,
-    red: `linear-gradient(to bottom, #ff0000, #dd0003, #bd0004, #9d0003, #7e0000)`,
-    yellow: `linear-gradient(to bottom, #ffff00, #c7cc03, #939b03, #646c02, #394000)`,
-  };
-
-  // const sectionStyle = {
-  //   background: backgroundGradients[tribe.tribe_color],
-  // };
   const { castaways } = episodeData;
 
   const getTribeTitle = (tribeName) => {
