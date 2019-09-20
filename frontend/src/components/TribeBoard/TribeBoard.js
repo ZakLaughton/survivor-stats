69447879dcb2ff8405bba7ea404318a1f10b2911
Zak Laughton <zlaughton@gmail.com>
2019-09-20 12:19:09 -0500

    fix: remove old responsive card styles

--- frontend/src/components/TribeBoard/TribeBoard.js
+++ frontend/src/components/TribeBoard/TribeBoard.js
@@ -7,53 +7,6 @@ import { FormerTribeHighlightContext } from "./FormerTribeHighlightContext";
 const castawayCardSizeSm = `110px`;
 
 const GlobalStyle = createGlobalStyle`
-
-  /* Avoid single-column castaway list */
-  /* w/ two tribes */
-  @media only screen and (min-width: 515px) and (max-width: 637px) {
-    .tribe-count-2 .tribe .castaway-card {
-      min-width: ${castawayCardSizeSm};
-      min-height: ${castawayCardSizeSm};
-      width: ${castawayCardSizeSm};
-      max-width: ${castawayCardSizeSm};
-      max-height: ${castawayCardSizeSm};
-    }
-
-    .tribe-count-2 .tribe .castaway-card h2 {
-      font-size: 1.3rem;
-    }
-  }
-
-  /* w/ three tribes */
-  @media only screen and (min-width: 766px) and (max-width: 957px) {
-    .tribe-count-3 .tribe .castaway-card {
-      min-width: ${castawayCardSizeSm};
-      min-height: ${castawayCardSizeSm};
-      width: ${castawayCardSizeSm};
-      max-width: ${castawayCardSizeSm};
-      max-height: ${castawayCardSizeSm};
-    }
-
-    .tribe-count-3 .tribe .castaway-card h2 {
-      font-size: 1.3rem;
-    }
-  }
-
-  /* w/ 4 tribes */
-  @media only screen and (min-width: 761px) and (max-width: 1075px) {
-    .tribe-count-4 .tribe .castaway-card {
-      min-width: ${castawayCardSizeSm};
-      min-height: ${castawayCardSizeSm};
-      width: ${castawayCardSizeSm};
-      max-width: ${castawayCardSizeSm};
-      max-height: ${castawayCardSizeSm};
-    }
-
-    .tribe-count-4 .tribe .castaway-card h2 {
-      font-size: 1.3rem;
-    }
-  }
-
   @media only screen and (min-width: 761px) and (max-width: 900px) {
     .tribe-count-4.active-tribes {
       flex-flow: row wrap !important;
@@ -65,20 +18,6 @@ const GlobalStyle = createGlobalStyle`
 
     .tribe-count-4 .extinction-island h1 {
       margin-bottom: 0;
-    }
-  }
-
-  @media only screen and (max-width: 761px) {
-    .tribe .castaway-card {
-      min-width: ${castawayCardSizeSm};
-      min-height: ${castawayCardSizeSm};
-      width: ${castawayCardSizeSm};
-      max-width: ${castawayCardSizeSm};
-      max-height: ${castawayCardSizeSm};
-    }
-
-    .tribe .castaway-card h2 {
-      font-size: 1.3rem;
     }
   }
 `;
