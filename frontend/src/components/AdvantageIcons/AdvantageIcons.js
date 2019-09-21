a4f7a8db31f6a059e15c32b981fa27d3acd4d0e3
Zak Laughton <zlaughton@gmail.com>
2019-09-21 10:55:20 -0500

    fix: put border around advantage icon

--- frontend/src/components/AdvantageIcons/AdvantageIcons.js
+++ frontend/src/components/AdvantageIcons/AdvantageIcons.js
@@ -1,35 +1,36 @@
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable global-require */
-import React from 'react';
-import './AdvantageIcons.css';
+import React from "react";
+import "./AdvantageIcons.css";
+import styled from "styled-components";
 
 const AdvantageIcons = ({ castaway }) => {
   const iconLibrary = {
-    'immunity idol': 'fas fa-shield-alt',
-    'idol nullifier': 'fas fa-ban',
-    'vote steal': 'fas fa-user-minus',
-    'legacy advantage': 'fas fa-scroll',
-    'extra vote': 'fas fa-plus-circle',
-    'fake idol': 'fab fa-pagelines',
-    'multi-choice advantage': 'fas fa-arrows-alt',
+    "immunity idol": `fas fa-shield-alt`,
+    "idol nullifier": `fas fa-ban`,
+    "vote steal": `fas fa-user-minus`,
+    "legacy advantage": `fas fa-scroll`,
+    "extra vote": `fas fa-plus-circle`,
+    "fake idol": `fab fa-pagelines`,
+    "multi-choice advantage": `fas fa-arrows-alt`,
   };
 
   const pngLibrary = {
-    'immunity idol': {
-      alt: 'Immunity Idol',
-      filename: 'immunity_idol.png',
+    "immunity idol": {
+      alt: `Immunity Idol`,
+      filename: `immunity_idol.png`,
     },
-    'fake immunity idol': {
-      alt: 'Fake Immunity Idol',
-      filename: 'fake_immunity_idol.png',
+    "fake immunity idol": {
+      alt: `Fake Immunity Idol`,
+      filename: `fake_immunity_idol.png`,
     },
-    'half immunity idol 1': {
-      alt: 'Half Immunity Idol',
-      filename: 'half_immunity_idol_1.png',
+    "half immunity idol 1": {
+      alt: `Half Immunity Idol`,
+      filename: `half_immunity_idol_1.png`,
     },
-    'half immunity idol 2': {
-      alt: 'Half Immunity Idol',
-      filename: 'half_immunity_idol_2.png',
+    "half immunity idol 2": {
+      alt: `Half Immunity Idol`,
+      filename: `half_immunity_idol_2.png`,
     },
   };
 
@@ -50,9 +51,9 @@ const AdvantageIcons = ({ castaway }) => {
       );
     }
     return (
-      <i className={`tooltip advantage ${iconLibrary[advantageName]}`}>
+      <StyledFontAwesome className={`tooltip advantage ${iconLibrary[advantageName]}`}>
         <span className="animated fadeIn tooltiptext">{advantageName}</span>
-      </i>
+      </StyledFontAwesome>
     );
   };
 
@@ -65,5 +66,9 @@ const AdvantageIcons = ({ castaway }) => {
     </div>
   );
 };
+
+const StyledFontAwesome = styled.i`
+  text-shadow: -1px 0 black, 0 2px black, 2px 0 black, 0 -1px black;
+`;
 
 export default AdvantageIcons;
