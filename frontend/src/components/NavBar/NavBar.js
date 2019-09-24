33c840ffedf0fa612d434da6ea903d1540ced4ee
Zak Laughton <zlaughton@gmail.com>
2019-09-23 21:04:32 -0500

    start navbar transition to styled-components

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import { PROD_BACKEND_URL } from "../../constants";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import "./NavBar.css";
 
 const NavBar = ({
@@ -13,8 +14,6 @@ const NavBar = ({
   history,
 }) => {
   const [seasonDirectory, setSeasonDirectory] = useState([])
-  console.log(`HIST>>>`, history);
-  console.log('SD>>>', seasonDirectory);
   let seasonTitle = `Loading...`;
   
     seasonTitle = seasonNumber && seasonDirectory.length > 0
@@ -44,22 +43,20 @@ const updateSeasonDirectory = async () => {
   const openDropdown = () => {
     document.querySelector(`.dropdown`).classList.add(`active`);
   };
-  console.log('!>>', seasonDirectory);
   return (
-    <header className="navbar" id="myTopnav">
+    <StyledNavBar className="navbar">
       {
-        <div
+        <SeasonSelector
           className="dropdown"
-          id="season-select"
           onMouseLeave={closeDropdown}
           onTouchEnd={openDropdown}
         >
           {(
             <div className="dropbtn" onMouseEnter={openDropdown}>
               <div className="season-title">{seasonTitle}</div>
-              <div className="season-title-icon">
+              <SeasonTitleIconContainer>
                 <i className="fas fa-caret-down" />
-              </div>
+              </SeasonTitleIconContainer>
             </div>
           )}
           <div className="dropdown-content">
@@ -77,23 +74,101 @@ const updateSeasonDirectory = async () => {
               </Link>
             ))}
           </div>
-        </div>
+        </SeasonSelector>
       }
 
-      <div className="episode-selector">
-        <i
-          className={`fas fa-caret-left episode-arrow left ${atEarliestEpisode() && `hidden`}`}
+      <EpisodeSelector>
+        <EpisodeArrow left hidden={atEarliestEpisode()}
+          className={`fas fa-caret-left`}
           onClick={decrementEpisode}
         />
-        <h2>{episodeNumber === 0 ? `START` : `EPISODE ${episodeNumber}`}</h2>
-        <i
-          className={`fas fa-caret-right episode-arrow right ${atLatestEpisode() && `hidden`}`}
+        <EpisodeTitle>{episodeNumber === 0 ? `START` : `EPISODE ${episodeNumber}`}</EpisodeTitle>
+        <EpisodeArrow right hidden={atLatestEpisode()}
+          className={`fas fa-caret-right episode-arrow right`}
           onClick={incrementEpisode}
         />
-      </div>
-      <h1>Survivor Stats</h1>
-    </header>
+      </EpisodeSelector>
+      <SiteTitle>Survivor Stats</SiteTitle>
+    </StyledNavBar>
   );
 };
+
+const StyledNavBar = styled.header`
+  background-color: #111;
+  z-index: 100;
+  display: grid;
+  grid-template-columns: 1fr auto 1fr;
+  position: sticky;
+  top: 0;
+  width: 100%;
+  align-items: center;
+  grid-template-areas: "season episode title";
+  font-family: Arial;
+`
+
+const SeasonSelector = styled.div`
+  grid-area: season;
+  height: 40px;
+`
+
+const SeasonTitle = styled.div`
+overflow: hidden;
+white-space: nowrap;
+text-overflow: ellipsis;
+display: inline-block;
+`
+
+const SeasonTitleIconContainer = styled.div`
+  padding-left: 8px;
+  font-size: .8em;
+  padding-right: 8px;
+`
+
+const EpisodeSelector = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 124px 1fr;
+  grid-template-areas: "left-arrow episode right-arrow";
+  align-items: center;
+  justify-self: center;
+  align-self: stretch;
+  height: 40px;
+  font-family: 'Londrina Solid', sans-serif;
+  font-weight: 400;
+  font-size: 1.4rem;
+  color: white;
+  user-select: none;
+  grid-area: episode;
+`
+
+const EpisodeTitle = styled.h2`
+display: inline;
+font-weight: 400;
+font-size: 1.8rem;
+justify-self: center;
+/* Override other h2 properties */
+margin-block-start: 0;
+margin-block-end: 0;
+grid-area: episode;
+`
+
+const EpisodeArrow = styled.i`
+  display: inline-block;
+  padding: 0px 9px;
+  font-size: 2rem;
+  justify-self: ${props => props.left ? `end` : `start`};
+  visibility: ${props => props.hidden ? `hidden` : `default`};
+  grid-area: ${props => props.left ? `left-arrow` : `right-arrow`};
+`
+
+const SiteTitle = styled.h1`
+  margin: 0px;
+  justify-self: end;
+  font-family: 'Survivant';
+  font-size: 1.5rem;
+  font-weight: 500;
+  color: rgb(255,255,255,0.8);
+  padding-right: 5px;
+  grid-area: title;
+`
 
 export default NavBar;
