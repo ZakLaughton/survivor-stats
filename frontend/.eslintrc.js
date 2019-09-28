e8ce9f25bfc1946c43d5b15a1d48928589db786e
Zak Laughton <zlaughton@gmail.com>
2019-09-28 17:02:37 -0500

    eslint update

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,14 +1,15 @@
 module.exports = {
-  extends: ["airbnb", "prettier"],
+  extends: ['airbnb', 'prettier'],
   rules: {
-    "linebreak-style": "off",
-    "react/jsx-filename-extension": "off",
-    "react/prop-types": 0,
-    "react/no-array-index-key": "off",
-    "import/prefer-default-export": "off",
+    'linebreak-style': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 0,
+    'react/no-array-index-key': 'off',
+    'import/prefer-default-export': 'off',
+    'no-magic-numbers': 'warn'
   },
   env: {
-    browser: true,
+    browser: true
   },
-  parser: "babel-eslint",
+  parser: 'babel-eslint'
 };
